d0c56ea065d7ab4260f7abb6129dbb0efb786e0a
Miguel Faraj <migueledwardfaraj@gmail.com>
2026-06-23 09:29:28 +0300

    Update common.js

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 // ---- Config ----
 // Google Apps Script Web App URL (deployed from the linked Google Sheet).
-const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbyX91Ln_tbXLDP0xLeYVDP3BWTOxiV7LxpoX6J0US8UaEshraWRSUn7TPWtMhAenrGf/exec";
+const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbykoEhPpcuC0woxJ6Za_XravOsGsxh3OummMFGu-waMI8dPbvaFb685r0ORjggjqwZW/exec";
 
 // ---- Session helpers ----
 function getSession() {
